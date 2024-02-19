3d28f3b93abb0603d33fc1861d30b84b945c7cc0
MDREZA110 <130495009+MDREZA110@users.noreply.github.com>
2024-02-19 10:05:39 +0530

    if statment"

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -13,30 +13,34 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
-  
-  void switchScreen(){
+
+  void switchScreen() {
     setState(() {
       activeScreen = 'question-screen';
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color.fromARGB(255, 62, 28, 122),
-                    Color.fromARGB(255, 23, 4, 54)
-                  ]),
-            ),
-            child: activeScreen == 'start-screen' ? StartScreen(switchScreen) :const QuestionsScreen())
-            ),
-     
+          body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.fromARGB(255, 62, 28, 122),
+                      Color.fromARGB(255, 23, 4, 54)
+                    ]),
+              ),
+              child: screenWidget)),
     );
   }
 }
