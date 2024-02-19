912d3ab782263ca47b0239b4788796e17622c075
MDREZA110 <130495009+MDREZA110@users.noreply.github.com>
2024-02-19 09:21:29 +0530

    first commit

---
+++ lib/question_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Questions extends StatefulWidget {
+  const Questions({super.key});
+
+  @override
+  State<Questions> createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("question Screen"),);
+  }
+}
